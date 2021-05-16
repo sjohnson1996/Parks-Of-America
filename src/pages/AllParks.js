505e64f3a9c17036491e5b1a1b03e949b43a4987
sjohnson96 <johnscott10k@gmail.com>
2021-05-16 15:09:25 -0400

    Added API pagination logic

--- src/pages/AllParks.js
+++ src/pages/AllParks.js
@@ -1,15 +1,45 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import '../App.css';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const AllParks = (props) => {
+
+    const key = process.env.REACT_APP_API_KEY;
+
+    const [parks, setParks] = useState([]);
+    const [totalResults, setTotalResults] = useState(0);
+    const [resultsNum, setResultsNum] = useState(0);
     
     useEffect(() => {
         props.grabRoute(props.match.path);
-    }, [])
+        axios.get(`https://developer.nps.gov/api/v1/parks?start=${resultsNum}&api_key=${key}`)
+            .then(res => {
+                console.log(res.data);
+                setTotalResults(res.data.total)
+                setParks(res.data);
+            })
+    }, [resultsNum]);
+
+    const next = () => {
+        setResultsNum(resultsNum + 50);
+    }
+
+    const previous = () => {
+        setResultsNum(resultsNum - 50);
+    }
 
     return (
         <div>
-            <h1>All Parks</h1>
+            <h1 className="all-parks-title">All Parks</h1>
+            {(resultsNum > 50) ? 
+                <button onClick={previous}>Previous</button>
+            : null}
+            {(resultsNum < totalResults - 50) ? 
+                <button onClick={next}>Next</button>
+            : null}
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import '../App.css';
+import flag from '../images/flag.svg';
 
 const Home = (props) => {
     
@@ -9,7 +10,10 @@ const Home = (props) => {
     
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="welcome-style">Welcome To...</h1>
+            <h1 className="parks-of-america">Parks of America</h1>
+            <p className="intro-style">With a total of 467 national parks across the United States, there is no shortage of opportunities to experience the natural beauty of this country. Take a journey across the Parks of America from the comfort of your home and get inspired to get outdoors!</p>
+            <img className="flag-style" src={flag} alt="American Flag"/>
         </div>
     );
 }
